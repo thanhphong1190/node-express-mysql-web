abf7f70504dd26bf7d6153e67a8a2154bd82f6d0
phongnet <thanhphong1190@gmail.com>
2020-04-12 22:15:59 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from "./components/header/Header";
+import Content from "./components/content/Content"
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Content/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/content/Content.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Content() {
+  return (
+    <div className="Content">
+      Content
+    </div>
+  );
+}
+
+export default Content;
